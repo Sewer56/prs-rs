b1253491cdaf721b8235cb38bc5dfd8de7ac8c12
Sewer56 <sewer56lol@googlemail.com>
2024-02-04 08:45:51 +0000

    Added: PGO Experiment

--- benches/my_benchmark/main.rs
+++ benches/my_benchmark/main.rs
@@ -18,8 +18,13 @@ use pprof::criterion::{Output, PProfProfiler};
 fn criterion_benchmark(c: &mut Criterion) {
     bench_estimate(c);
     bench_decompress(c);
-    //bench_create_dict(c); // Disabled so we avoid this in PGO.
     bench_compress_file(c);
+
+    // Excluded from PGO.
+    #[cfg(not(feature = "pgo"))]
+    {
+        bench_create_dict(c);
+    }
 }
 
 #[cfg(not(target_os = "windows"))]
